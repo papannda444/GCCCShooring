c88b3ed14c33beb251dc694c8e03bdacba942ff4
papannda444 <papannda444@gmail.com>
2019-03-03 21:28:43 +0900

    Add SpaceShipProtocol

---
+++ shooting-game/SpaceShip/SpaceShipState.swift
@@ -0,0 +1,28 @@
+//
+//  SpaceShipState.swift
+//  shooting-game
+//
+//  Created by papannda444 on 2019/03/03.
+//  Copyright © 2019 三野田脩. All rights reserved.
+//
+
+import Foundation
+
+enum SpaceShipState: String {
+    case normal
+    case speed
+    case stone
+
+    init() {
+        self = .normal
+    }
+
+    mutating func shipPowerUp(itemType: PowerItem.ItemType) {
+        switch itemType {
+        case .speed:
+            self = .speed
+        case .stone:
+            self = .stone
+        }
+    }
+}
