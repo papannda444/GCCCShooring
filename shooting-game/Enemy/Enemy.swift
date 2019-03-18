51472addc7b8d5f456c031bcd96cd557309f730a
papannda444 <papannda444@gmail.com>
2019-03-18 13:20:08 +0900

    Update enemy move

--- shooting-game/Enemy/Enemy.swift
+++ shooting-game/Enemy/Enemy.swift
@@ -36,19 +36,55 @@ extension Enemy {
 
     func createEnemyMovement(displayViewFrame frame: CGRect) {
         let positionX = frame.width * (CGFloat.random(in: 0...1) - 0.5)
-        let topToBottom = SKAction.sequence([
-            SKAction.move(to: CGPoint(x: positionX, y: frame.height / 2 + 100), duration: 0.0),
-            SKAction.rotate(toAngle: CGFloat.degreeToRadian(degree: 180), duration: 0.0),
-            SKAction.moveTo(y: -frame.height / 2 - 100, duration: 4.0),
+        var path = CGMutablePath()
+        path.move(to: CGPoint(x: positionX, y: frame.height / 2 + 100))
+        path.addLine(to: CGPoint(x: positionX, y: -frame.height / 2 - 100))
+        path.addLine(to: CGPoint(x: positionX, y: frame.height / 2 + 100))
+        let downUp = SKAction.sequence([
+            .follow(path, duration: 8.0),
             SKAction.removeFromParent()
         ])
-        enemyMove.append(topToBottom)
-        let bottomToTop = SKAction.sequence([
-            SKAction.move(to: CGPoint(x: positionX, y: -frame.height / 2 - 100), duration: 0.0),
-            SKAction.moveTo(y: frame.height / 2 + 100, duration: 4.0),
-            SKAction.removeFromParent()
+        enemyMove.append(downUp)
+        path = CGMutablePath()
+        path.move(to: CGPoint(x: frame.width / 2 + 100, y: frame.height / 2))
+        path.addLine(to: CGPoint(x: frame.width / 4, y: 0))
+        path.addLine(to: CGPoint(x: -frame.width / 4, y: 0))
+        path.addLine(to: CGPoint(x: -frame.width / 2 - 100, y: frame.height / 2))
+        let vShaped = SKAction.sequence([
+            .follow(path, duration: 4.0),
+            .removeFromParent()
         ])
-        enemyMove.append(bottomToTop)
+        enemyMove.append(vShaped)
+        path = CGMutablePath()
+        path.move(to: CGPoint(x: -frame.width / 2 - 100, y: frame.height / 2))
+        path.addLine(to: CGPoint(x: -frame.width / 4, y: 0))
+        path.addLine(to: CGPoint(x: frame.width / 4, y: 0))
+        path.addLine(to: CGPoint(x: frame.width / 2 + 100, y: frame.height / 2))
+        let vShapedReverse = SKAction.sequence([
+            .follow(path, duration: 4.0),
+            .removeFromParent()
+        ])
+        enemyMove.append(vShapedReverse)
+        path = CGMutablePath()
+        path.move(to: CGPoint(x: -frame.width / 2 - 100, y: frame.height / 2))
+        path.addLine(to: CGPoint(x: frame.width / 2, y: frame.height / 4))
+        path.addLine(to: CGPoint(x: -frame.width / 2, y: 0))
+        path.addLine(to: CGPoint(x: frame.width / 2 + 100, y: -frame.height / 4))
+        let zigzag = SKAction.sequence([
+            .follow(path, duration: 4.0),
+            .removeFromParent()
+        ])
+        enemyMove.append(zigzag)
+        path = CGMutablePath()
+        path.move(to: CGPoint(x: frame.width / 2 + 100, y: frame.height / 2))
+        path.addLine(to: CGPoint(x: -frame.width / 2, y: frame.height / 4))
+        path.addLine(to: CGPoint(x: frame.width / 2, y: 0))
+        path.addLine(to: CGPoint(x: -frame.width / 2 - 100, y: -frame.height / 4))
+        let zigzagReverse = SKAction.sequence([
+            .follow(path, duration: 4.0),
+            .removeFromParent()
+        ])
+        enemyMove.append(zigzagReverse)
     }
 
     func isShipState(equal state: EnemyState) -> Bool {
